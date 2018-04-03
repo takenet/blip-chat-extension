098e71dd08ac7ff18c14873ecc0119d4b6e7ed38
Luiz Guilherme Correa Louro <luizg@takenet.com.br>
2018-04-03 17:54:06 -0300

    fix(Logs): Remove logs

--- src/PostMessageUtils.js
+++ src/PostMessageUtils.js
@@ -1,8 +1,6 @@
 export class PostMessageUtils {
   static sendPostMessage(message) {
     const parentDomain = window.location.ancestorOrigins[0]
-    console.log('parentDomain', parentDomain, 'message', message)
-    console.log('Window parent', window.parent)
 
     window.parent.postMessage(message, parentDomain)
   }
