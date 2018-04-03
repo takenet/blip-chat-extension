f0658f3c7532c42e5af20be5f2ba6a953d937ad0
Luiz Guilherme Correa Louro <luizg@takenet.com.br>
2018-04-03 17:45:05 -0300

    fix(Post message): Fix wrong parent domain on post mesage

--- src/PostMessageUtils.js
+++ src/PostMessageUtils.js
@@ -1,6 +1,9 @@
 export class PostMessageUtils {
   static sendPostMessage(message) {
-    const parentDomain = document.referrer
+    const parentDomain = window.location.ancestorOrigins[0]
+    console.log('parentDomain', parentDomain, 'message', message)
+    console.log('Window parent', window.parent)
+
     window.parent.postMessage(message, parentDomain)
   }
 }
