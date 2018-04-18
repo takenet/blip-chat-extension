39367171cdc4b3f6ccaa64bf1249e56f2b23e488
Victor Balbo <victorb@take.net>
2018-04-18 17:34:36 -0300

    fix(PostMessage): Fix postmessage for IE

--- src/PostMessageUtils.js
+++ src/PostMessageUtils.js
@@ -1,6 +1,6 @@
 export class PostMessageUtils {
   static sendPostMessage(message) {
-    const parentDomain = window.location.ancestorOrigins[0] || 'https://chat.blip.ai'
+    const parentDomain = window.location.ancestorOrigins ? window.location.ancestorOrigins[0] : 'https://chat.blip.ai'
     window.parent.postMessage(message, parentDomain)
   }
 }
